1e41e83baee2b986b72b960fd7b5f29acd744dd9
Eric Dallo <ericdallo06@hotmail.com>
2024-07-01 12:09:44 -0300

    Remove logger

--- src/main/clojure/com/github/clojure_lsp/intellij/workspace_edit.clj
+++ src/main/clojure/com/github/clojure_lsp/intellij/workspace_edit.clj
@@ -4,7 +4,6 @@
    [com.github.clojure-lsp.intellij.editor :as editor]
    [com.github.clojure-lsp.intellij.file-system :as file-system]
    [com.github.ericdallo.clj4intellij.app-manager :as app-manager]
-   [com.github.ericdallo.clj4intellij.logger :as logger]
    [com.github.ericdallo.clj4intellij.util :as util]))
 
 (set! *warn-on-reflection* true)
@@ -16,7 +15,6 @@
 (defmethod lsp-client/show-document :default [{:keys [project]} {:keys [uri take-focus selection]}]
   (app-manager/invoke-later!
    {:invoke-fn (fn []
-                 (logger/info "--------->" uri (util/uri->v-file uri))
                  (let [editor (util/uri->editor uri project (boolean take-focus))]
                    (.moveToOffset (.getCaretModel editor)
                                   (editor/document+position->offset (:start selection) (.getDocument editor)))))})
