d6d926308cf9cf59924032f2bf2ef91b4b985e9a
Eric Dallo <ericdallo06@hotmail.com>
2025-01-29 14:29:08 -0300

    improve semantic tokens

---
+++ src/main/clojure/com/github/clojure_lsp/intellij/extension/semantic_token_provider.clj
@@ -0,0 +1,36 @@
+(ns com.github.clojure-lsp.intellij.extension.semantic-token-provider
+  (:gen-class
+   :name com.github.clojure_lsp.intellij.extension.ClojureSemanticTokensColorsProvider
+   :implements [com.redhat.devtools.lsp4ij.features.semanticTokens.SemanticTokensColorsProvider])
+  (:import
+   [com.intellij.openapi.editor.colors TextAttributesKey]
+   [com.intellij.psi PsiFile]
+   [com.redhat.devtools.lsp4ij.features.semanticTokens DefaultSemanticTokensColorsProvider SemanticTokensHighlightingColors]
+   [java.awt Font]
+   [java.util List]
+   [org.eclipse.lsp4j SemanticTokenModifiers SemanticTokenTypes]))
+
+(def ^:private default-provider (DefaultSemanticTokensColorsProvider.))
+
+(defn ^:private modifier? [modifier token-modifiers]
+  (some #(= modifier %) token-modifiers))
+
+(defn make-bold [^TextAttributesKey text-attribute-key]
+  (TextAttributesKey/createTextAttributesKey
+   (str (.getExternalName text-attribute-key) "_BOLD")
+   (doto (.clone (.getDefaultAttributes text-attribute-key))
+     (.setFontType Font/BOLD))))
+
+(def ^:private bold-function-declaration
+  (make-bold SemanticTokensHighlightingColors/FUNCTION_DECLARATION))
+
+(defn -getTextAttributesKey [_ ^String token-type ^List token-modifiers ^PsiFile psi-file]
+  (condp = token-type
+    SemanticTokenTypes/Namespace SemanticTokensHighlightingColors/STATIC_PROPERTY
+    SemanticTokenTypes/Function (if (modifier? SemanticTokenModifiers/Definition token-modifiers)
+                                  bold-function-declaration
+                                  SemanticTokensHighlightingColors/MACRO)
+    SemanticTokenTypes/Type SemanticTokensHighlightingColors/STATIC_PROPERTY
+    SemanticTokenTypes/Variable SemanticTokensHighlightingColors/READONLY_VARIABLE
+    SemanticTokenTypes/Keyword SemanticTokensHighlightingColors/NUMBER
+    (.getTextAttributesKey default-provider token-type token-modifiers psi-file)))
