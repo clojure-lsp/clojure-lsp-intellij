0c28fcd75c8af0f9b374035f155b9af354567882
Arthur Fücher <arthurfucher@gmail.com>
2025-04-01 14:26:20 -0300

    try to execute slurp action

--- src/test/clojure/com/github/clojure_lsp/intellij/foo_test.clj
+++ src/test/clojure/com/github/clojure_lsp/intellij/foo_test.clj
@@ -4,7 +4,9 @@
    [com.github.ericdallo.clj4intellij.app-manager :as app-manager]
    [com.github.ericdallo.clj4intellij.test :as clj4intellij.test])
   (:import
-   [com.intellij.openapi.wm WindowManager]))
+   [com.intellij.openapi.wm WindowManager]
+   [com.intellij.openapi.actionSystem ActionManager]
+   [com.intellij.ide DataManager]))
 
 (set! *warn-on-reflection* true)
 
@@ -12,6 +14,22 @@
 (defn get-status-bar-widget [project widget-id]
   (let [status-bar (.. (WindowManager/getInstance) (getStatusBar project))]
     (.getWidget status-bar widget-id)))
+
+
+(defn run-editor-action [action-id project]
+  (let [action (.getAction (ActionManager/getInstance) action-id)
+        context (.getDataContext (DataManager/getInstance))]
+
+
+    (println "Running action:" action-id)
+    (println "Action:" action)
+    (app-manager/write-command-action
+     project
+     (fn []
+       (.actionPerformed
+        action
+        (com.intellij.openapi.actionSystem.AnActionEvent/createFromDataContext action-id nil context))))))
+
 
 (deftest foo-test
   (let [project-name "clojure.core"
@@ -27,7 +45,11 @@
      project
      (fn [] (.openFileInEditor fixture clj-file)))
 
+
+
     (clj4intellij.test/dispatch-all)
+    (println "OLAAAA >> ")
+    (run-editor-action "ClojureLSP.ForwardSlurp" project)
 
     @(app-manager/invoke-later!
       {:invoke-fn (fn []
