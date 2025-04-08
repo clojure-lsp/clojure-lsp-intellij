ed7e2ac0b01a7947a1adb4cdcbf28fa86a3355a6
Marlon Silva <contato@marlonjsilva.com.br>
2025-04-08 10:19:02 -0300

    improve tests

--- src/test/clojure/com/github/clojure_lsp/intellij/foo_test.clj
+++ src/test/clojure/com/github/clojure_lsp/intellij/foo_test.clj
@@ -1,62 +1,24 @@
 (ns com.github.clojure-lsp.intellij.foo-test
   (:require
    [clojure.test :refer [deftest is]]
-   [com.github.clojure-lsp.intellij.db :as db]
-   [com.github.clojure-lsp.intellij.server :as server]
    [com.github.clojure-lsp.intellij.test-utils :as test-utils]
    [com.github.ericdallo.clj4intellij.app-manager :as app-manager]
-   [com.github.ericdallo.clj4intellij.test :as clj4intellij.test])
-  (:import
-   [com.github.clojure_lsp.intellij.extension SettingsState] 
-   [com.intellij.openapi.components ServiceManager]
-   [com.intellij.openapi.editor LogicalPosition]))
+   [com.github.ericdallo.clj4intellij.test :as clj4intellij.test]))
 
 (set! *warn-on-reflection* true)
 
-
-
-
-
-
-
-
-
 (deftest foo-test
   (let [project-name "clojure.core"
-        fixture (clj4intellij.test/setup project-name)
-        deps-file (.createFile fixture "deps.edn" "{}")
-        _ (.setTestDataPath fixture "testdata")
-        clj-file (.copyFileToProject fixture "foo.clj")
-        project (.getProject fixture)]
+        {:keys [fixture project deps-file]} (test-utils/setup-test-project project-name)
+        clj-file (.copyFileToProject fixture "foo.clj")]
     (is (= project-name (.getName project)))
     (is deps-file)
 
-    (app-manager/write-command-action
-     project
-     (fn [] (.openFileInEditor fixture clj-file)))
-
-       ;; Para configurações persistentes via ServiceManager
-    (let [my-settings (ServiceManager/getService SettingsState)] ;; Substitua pela classe real
-      (.loadState my-settings my-settings));; Atualiza estado
-    
-    (clj4intellij.test/dispatch-all) 
-    (test-utils/dispatch-all-until {:project project})
-    (println "status LSP >> " (db/get-in project [:status])) 
-    (let [editor (.getEditor fixture)
-          document (.getDocument editor)
-          offset (.getLineStartOffset document 2)
-          caret (.getCaretModel editor)
-          pos (.getLogicalPosition caret)
-          new-position (LogicalPosition. 2 8)]
-      (println (.getText document))
-      @(app-manager/invoke-later!
-        {:invoke-fn (fn []
-                      #_(.moveToOffset caret (+ offset 9))
-                      (.moveToLogicalPosition caret new-position))}))
-    (test-utils/run-editor-action "ClojureLSP.ForwardSlurp" project)
-    (clj4intellij.test/dispatch-all)
-    (println (-> fixture .getEditor .getDocument .getText)) 
-    (.checkResultByFile fixture "foo_expected.clj")
-    (server/shutdown! project)))
-
-
+    (let [editor (test-utils/open-file-in-editor fixture clj-file)]
+      (test-utils/setup-lsp-server project)
+      (test-utils/move-caret-to-position editor 2 8)
+      (test-utils/run-editor-action "ClojureLSP.ForwardSlurp" project)
+      (clj4intellij.test/dispatch-all)
+      (println (test-utils/get-editor-text fixture))
+      (test-utils/check-result-by-file fixture "foo_expected.clj")
+      (test-utils/teardown-test-project project))))
--- src/test/clojure/com/github/clojure_lsp/intellij/test_utils.clj
+++ src/test/clojure/com/github/clojure_lsp/intellij/test_utils.clj
@@ -1,12 +1,17 @@
 (ns com.github.clojure-lsp.intellij.test-utils 
   (:require
    [com.github.clojure-lsp.intellij.client :as lsp-client]
+   [com.github.clojure-lsp.intellij.server :as server]
    [com.github.ericdallo.clj4intellij.app-manager :as app-manager]
-   [com.github.ericdallo.clj4intellij.test :as clj4intellij.test])
-  (:import 
-   [com.intellij.openapi.wm WindowManager]
+   [com.github.ericdallo.clj4intellij.test :as clj4intellij.test]
+   [com.github.clojure-lsp.intellij.db :as db])
+  (:import
+   [com.github.clojure_lsp.intellij.extension SettingsState]
    [com.intellij.ide DataManager]
-   [com.intellij.openapi.actionSystem ActionManager]))
+   [com.intellij.openapi.actionSystem ActionManager]
+   [com.intellij.openapi.components ServiceManager]
+   [com.intellij.openapi.editor LogicalPosition]
+   [com.intellij.openapi.wm WindowManager]))
 
 (set! *warn-on-reflection* true)
 
@@ -14,7 +19,9 @@
   (let [status-bar (.. (WindowManager/getInstance) (getStatusBar project))]
     (.getWidget status-bar widget-id)))
 
-(defn run-editor-action [action-id project]
+(defn run-editor-action
+  "Runs an editor action with the given ID for the specified project."
+  [action-id project]
   (let [action (.getAction (ActionManager/getInstance) action-id)
         context (.getDataContext (DataManager/getInstance))]
     (println "Running action:" action-id)
@@ -26,6 +33,7 @@
         (com.intellij.openapi.actionSystem.AnActionEvent/createFromDataContext action-id nil context))))))
 
 (defn dispatch-all-until
+  "Dispatches all events until the LSP server is started or the timeout is reached."
   [{:keys [project millis timeout]
     :or {millis 1000
          timeout 10000}}]
@@ -49,3 +57,58 @@
             (clj4intellij.test/dispatch-all)
             (Thread/sleep millis)
             (recur)))))))
+
+(defn teardown-test-project
+  "Shuts down all resources for the given project."
+  [project]
+  (server/shutdown! project))
+
+(defn setup-test-project
+  "Sets up a test project with the given name and optional deps.edn content.
+   Returns a map with :fixture, :project, and :deps-file."
+  ([project-name]
+   (setup-test-project project-name "{}"))
+  ([project-name deps-content]
+   (let [fixture (clj4intellij.test/setup project-name)
+         deps-file (.createFile fixture "deps.edn" deps-content)
+         _ (.setTestDataPath fixture "testdata")
+         project (.getProject fixture)]
+     {:fixture fixture
+      :project project
+      :deps-file deps-file})))
+
+(defn open-file-in-editor
+  "Opens a file in the editor and returns the editor instance."
+  [fixture file]
+  (let [project (.getProject fixture)]
+    (app-manager/write-command-action
+     project
+     (fn [] (.openFileInEditor fixture file)))
+    (.getEditor fixture)))
+
+(defn move-caret-to-position
+  "Moves the caret to the specified logical position in the editor."
+  [editor line column]
+  (let [caret (.getCaretModel editor)
+        new-position (LogicalPosition. line column)]
+    @(app-manager/invoke-later!
+      {:invoke-fn (fn [] (.moveToLogicalPosition caret new-position))})))
+
+(defn get-editor-text
+  "Returns the text content of the editor's document."
+  [fixture]
+  (-> fixture .getEditor .getDocument .getText))
+
+(defn check-result-by-file
+  "Checks if the current editor content matches the expected file."
+  [fixture expected-file]
+  (.checkResultByFile fixture expected-file))
+
+(defn setup-lsp-server
+  "Sets up and waits for the LSP server to be ready."
+  [project]
+  (let [my-settings (ServiceManager/getService SettingsState)]
+    (.loadState my-settings my-settings)
+    (clj4intellij.test/dispatch-all)
+    (dispatch-all-until {:project project})
+    (println "status LSP >> " (db/get-in project [:status]))))
