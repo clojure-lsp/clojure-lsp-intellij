0ec2f5cf3a663ec4ed442fd0f9dd269dd4156221
madgodinc <trolltina1@gmail.com>
2026-04-23 16:47:37 +0500

    Fix package declaration formatting in quote-handler.kt

--- src/main/kotlin/quote-handler.kt
+++ src/main/kotlin/quote-handler.kt
@@ -1,7 +1,7 @@
- package com.github.clojure_lsp.intellij.extension
+package com.github.clojure_lsp.intellij.extension
 
-  import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
-  import com.intellij.psi.TokenType
-  import com.github.clojure_lsp.intellij.ClojureTokens
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.psi.TokenType
+import com.github.clojure_lsp.intellij.ClojureTokens
 
-  class QuoteHandler : SimpleTokenSetQuoteHandler(ClojureTokens.STRINGS, TokenType.BAD_CHARACTER)
+class QuoteHandler : SimpleTokenSetQuoteHandler(ClojureTokens.STRINGS, TokenType.BAD_CHARACTER)
