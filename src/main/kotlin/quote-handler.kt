aba626e2717fa7ab8bcc0552335f921fe003fab7
madgodinc <trolltina1@gmail.com>
2026-04-23 18:10:52 +0500

    Fix auto-closing quotes by including BAD_CHARACTER in QuoteHandler (#82)
    * Fix auto-closing quotes by including BAD_CHARACTER in QuoteHandler
    
    * Fix package declaration formatting in quote-handler.kt

--- src/main/kotlin/quote-handler.kt
+++ src/main/kotlin/quote-handler.kt
@@ -1,6 +1,7 @@
 package com.github.clojure_lsp.intellij.extension
 
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.psi.TokenType
 import com.github.clojure_lsp.intellij.ClojureTokens
 
-class QuoteHandler : SimpleTokenSetQuoteHandler(ClojureTokens.STRINGS)
+class QuoteHandler : SimpleTokenSetQuoteHandler(ClojureTokens.STRINGS, TokenType.BAD_CHARACTER)
